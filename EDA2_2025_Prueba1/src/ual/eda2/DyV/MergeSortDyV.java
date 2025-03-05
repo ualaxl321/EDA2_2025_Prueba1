0e50768c51c6cbe914b3afc591dad34bc629e25b
ANDRÉS XANDRI LÓPEZ <ualaxl321@inlumine.ual.es>
2025-03-05 09:44:33 +0100

    Convierto MergeSort al cristiano

--- EDA2_2025_Prueba1/src/ual/eda2/DyV/MergeSortDyV.java
+++ EDA2_2025_Prueba1/src/ual/eda2/DyV/MergeSortDyV.java
@@ -2,30 +2,30 @@ package ual.eda2.DyV;
 
 public class MergeSortDyV {
 
-	// Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
+	// Fusiona dos submatrices de arr[].
+	// La primera submatriz es arr[l..m]
+	// La segunda submatriz es arr[m+1..r]
     public static void merge(int arr[], int l, int m, int r) {
-        // Find sizes of two subarrays to be merged
+    	// Encuentra los tamaños de dos submatrices que se fusionarán
         int n1 = m - l + 1;
         int n2 = r - m;
   
-        /* Create temp arrays */
+        /* Crear matrices temporales */
         int L[] = new int[n1];
         int R[] = new int[n2];
   
-        /*Copy data to temp arrays*/
+        /*Copiar datos a matrices temporales*/
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
   
-        /* Merge the temp arrays */
+        /* Fusionar las matrices temporales */
   
-        // Initial indexes of first and second subarrays
+        // Índices iniciales de los subconjuntos primero y segundo
         int i = 0, j = 0;
   
-        // Initial index of merged subarray array
+        // Índice inicial de la matriz de subconjuntos fusionados
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -39,14 +39,14 @@ public class MergeSortDyV {
             k++;
         }
   
-        /* Copy remaining elements of L[] if any */
+        /* Copiar los elementos restantes de L[] si los hay */
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
   
-        /* Copy remaining elements of R[] if any */
+        /* Copiar los elementos restantes de R[] si los hay */
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -54,63 +54,63 @@ public class MergeSortDyV {
         }
     }
   
-    // Main function that sorts arr[l..r] using
+    // Función principal que ordena arr[l..r] usando
     // merge()
-    public static void mergeSortRecursive(int arr[], int l, int r) {
+    public static void mergeSortRecursivo(int arr[], int l, int r) {
         if (l < r) {
-            // Find the middle point
-            //int m = l + (r - l) / 2;
+        	// Encuentra el punto medio
+        	//int m = l + (r - l) / 2;
         	int m = (l + r) / 2;
   
-            // Sort first and second halves
-            mergeSortRecursive(arr, l, m);
-            mergeSortRecursive(arr, m + 1, r);
+        	// Ordenar la primera y la segunda mitad
+            mergeSortRecursivo(arr, l, m);
+            mergeSortRecursivo(arr, m + 1, r);
   
             // Merge the sorted halves
             merge(arr, l, m, r);
         }
     }
     
-    /* Iterative mergesort function to sort arr[0...n-1] */
-    public static void mergeSortIterative(int arr[], int n) {
+    /* Función de combinación iterativa para ordenar arr[0...n-1] */
+    public static void mergeSortIterativo(int arr[], int n) {
           
-        // For current size of subarrays to
-        // be merged curr_size varies from 
-        // 1 to n/2
-        int curr_size; 
+    	// Para el tamaño actual de las submatrices que se van a fusionar
+    	// tamaño_inicial varía de
+    	// 1 a n/2
+        int tamaño_inicial; 
                       
-        // For picking starting index of 
-        // left subarray to be merged
-        int left_start;
+	    // Para seleccionar el índice inicial de
+	    // la submatriz izquierda que se fusionará
+        int indice_submatriz_izquierda;
                           
           
-        // Merge subarrays in bottom up 
-        // manner. First merge subarrays 
-        // of size 1 to create sorted 
-        // subarrays of size 2, then merge
-        // subarrays of size 2 to create 
-        // sorted subarrays of size 4, and
-        // so on.
-        for (curr_size = 1; curr_size <= n-1; curr_size = 2*curr_size) {
+		// Fusionar submatrices de abajo a arriba
+		// Primero fusionar submatrices
+		// de tamaño 1 para crear
+		// submatrices ordenadas de tamaño 2, luego fusionar
+		// submatrices de tamaño 2 para crear
+		// submatrices ordenadas de tamaño 4, y
+		// así sucesivamente.
+        for (tamaño_inicial = 1; tamaño_inicial <= n-1; tamaño_inicial = 2*tamaño_inicial) {
               
-            // Pick starting point of different
-            // subarrays of current size
-            for (left_start = 0; left_start < n-1; left_start += 2*curr_size) {
-                // Find ending point of left 
-                // subarray. mid+1 is starting 
-                // point of right
-                int mid = Math.min(left_start + curr_size - 1, n-1);
+        	// Elija el punto de inicio de diferentes
+        	// submatrices del tamaño actual
+            for (indice_submatriz_izquierda = 0; indice_submatriz_izquierda < n-1; indice_submatriz_izquierda += 2*tamaño_inicial) {
+            	// Encuentra el punto final del subarreglo izquierdo
+            	// . mitad+1 es el punto inicial
+            	// del subarreglo derecho
+                int mitad = Math.min(indice_submatriz_izquierda + tamaño_inicial - 1, n-1);
           
-                int right_end = Math.min(left_start + 2*curr_size - 1, n-1);
+                int indice_submatriz_derecha = Math.min(indice_submatriz_izquierda + 2*tamaño_inicial - 1, n-1);
           
-                // Merge Subarrays arr[left_start...mid]
-                // & arr[mid+1...right_end]
-                merge(arr, left_start, mid, right_end);
+	            // Fusionar submatrices arr[indice_submatriz_izquierda...mitad]
+                // & arr[mitad+1...indice_submatriz_derecha]
+                merge(arr, indice_submatriz_izquierda, mitad, indice_submatriz_derecha);
             }
         }
     }
   
-    /* A utility function to print array of size n */
+    /* Una función de utilidad para imprimir una matriz de tamaño n */
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; ++i)
             System.out.print(arr[i] + " ");
@@ -121,16 +121,16 @@ public class MergeSortDyV {
 		int arr1[] = { 12, 11, 13, 5, 6, 7, 10, 4, 9 };
 		int arr2[] = { 12, 11, 13, 5, 6, 7, 10, 4, 9 };
 		  
-        System.out.println("Given Array");
+        System.out.println("Array dado");
         printArray(arr1);
   
-        mergeSortRecursive(arr1, 0, arr1.length - 1);
-        System.out.println("\nSorted array");
+        mergeSortRecursivo(arr1, 0, arr1.length - 1);
+        System.out.println("\nArray ordenado");
         printArray(arr1);
 
-        System.out.println("\n\nGiven Array");
+        System.out.println("\n\nArray dado");
         printArray(arr2);
-        mergeSortIterative(arr2, arr2.length);
+        mergeSortIterativo(arr2, arr2.length);
   
         System.out.println("\nSorted array");
         printArray(arr2);
